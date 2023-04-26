34040d2998e78b053ed44f40ba93f18ad70e3529
Joshua <23joshtc@gmail.com>
2023-04-26 14:46:24 -0400

    created variables with all the id's to store info from html. Used .getElementById function with html id's so that i can alter and manipulate element properties

--- script.js
+++ script.js
@@ -20,3 +20,13 @@ var quizQuestions = [
         answer: "html body"
     }
   ];
+
+  var quizContainer = document.getElementById("quiz-container");
+  var startBtn = document.getElementById("start-btn");
+  var submitBtn = document.getElementById("submit-btn");
+  var scoreContainer = document.getElementById("score-container");
+  var saveBtn = document.getElementById("save-btn");
+  var questionEl = document.getElementById("question");
+  var answerEl = document.getElementById("answer");
+  var scoreEl = document.getElementById("score");
+  var initialsEl = document.getElementById("initials");
