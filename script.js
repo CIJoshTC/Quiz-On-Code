935b2532998fa7d102f192c2bf9fb4e99842dba8
Joshua <23joshtc@gmail.com>
2023-04-26 14:32:57 -0400

    created array in java containing questions and answers

---
+++ script.js
@@ -0,0 +1,22 @@
+var quizQuestions = [
+    {
+        question: "What do we type to test run a code in Java?",
+        answer: "console.log"
+    },
+    {
+        question: "What programming language controls the look of a webpage?",
+        answer: "CSS"
+    },
+    {
+        question: "what puncuation is used to seperate code in java and css",
+        answer: "semicolon"
+    },
+    {
+        question: "what punctuation holds the arguments to a function",
+        answer: "parenthesis"
+    },
+    {
+        question: "where should java code be linked",
+        answer: "html body"
+    }
+  ];
